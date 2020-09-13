711ab0f7c5fa1e0884181b0e7fa23a854480ffc5
unknown <andersonlantunes@gmail.com>
2020-09-13 16:08:38 -0300

    Fixed transition bug of zero seconds

--- Day 2 -JS  + CSS Clock/script.js
+++ Day 2 -JS  + CSS Clock/script.js
@@ -8,13 +8,15 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(seconds);
+  seconds == 0 ? secondHand.classList.add("no-transition") : secondHand.classList.remove("no-transition");
 
   const minutes = now.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = (hours / 60) * 360 + 90;
+  const hoursDegrees = (hours / 60) * 360 + (minutes / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
